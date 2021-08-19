4eecc014863c72f4f0b614c4d15b77db646c4ba0
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-08-20 00:46:02 +0530

    download manager

--- codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/downloadmanager/android/MainActivity.kt
+++ codebase/android/app/src/main/java/com/makeappssimple/abhimanyu/downloadmanager/android/MainActivity.kt
@@ -1,11 +1,111 @@
 package com.makeappssimple.abhimanyu.downloadmanager.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var downloadID: Long = 0
+    private val onDownloadComplete: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            val id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if (downloadID == id) {
+                Toast.makeText(this@MainActivity, "Download Completed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Log.e("Abhi", "Permission is granted")
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                123
+            )
+        }
+
+        registerReceiver(onDownloadComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        findViewById<Button>(R.id.button).setOnClickListener { beginDownload() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(onDownloadComplete)
+    }
+
+    private fun beginDownload() {
+        val pdfUrl =
+            "https://www.eurofound.europa.eu/sites/default/files/ef_publication/field_ef_document/ef1710en.pdf"
+        val imageUrl = "https://pbs.twimg.com/media/Ez-AaifWYAIiFSQ.jpg"
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        val request =
+            DownloadManager.Request(Uri.parse(imageUrl))
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN) // Visibility of the download Notification
+                .setTitle("Download manager sample")
+                .setDescription("Downloading")
+                .setDestinationInExternalPublicDir("/Data", "test.jpg")
+        downloadID = downloadManager.enqueue(request)
+
+        /*
+        TODO: Add progress bar
+        // using query method
+        var finishDownload = false
+        var progress: Int
+        while (!finishDownload) {
+            val cursor = downloadManager.query(Query().setFilterById(downloadID))
+            if (cursor.moveToFirst()) {
+                val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                when (status) {
+                    DownloadManager.STATUS_FAILED -> {
+                        finishDownload = true
+                    }
+                    DownloadManager.STATUS_PAUSED -> {
+                    }
+                    DownloadManager.STATUS_PENDING -> {
+                    }
+                    DownloadManager.STATUS_RUNNING -> {
+                        val total =
+                            cursor.getLong(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
+                        if (total >= 0) {
+                            val downloaded =
+                                cursor.getLong(cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
+                            progress = (downloaded * 100L / total).toInt()
+                            // if you use downloadmanger in async task, here you can use like this to display progress.
+                            // Don't forget to do the division in long to get more digits rather than double.
+                            //  publishProgress((int) ((downloaded * 100L) / total));
+                        }
+                    }
+                    DownloadManager.STATUS_SUCCESSFUL -> {
+                        progress = 100
+                        // if you use aysnc task
+                        // publishProgress(100);
+                        finishDownload = true
+                        Toast.makeText(this@MainActivity, "Download Completed", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+        }
+        */
     }
 }
